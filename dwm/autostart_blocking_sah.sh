a22ebaff2ef77453b95c6b1076fefb7407eae7fb
self <george@gabbott.dev>
2022-09-19 11:36:53 +0100

    Check dunst/dwmblocks aren't running before start

--- dwm/autostart_blocking_sah.sh
+++ dwm/autostart_blocking_sah.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-feh --bg-scale $(shuf -n1 -e ~/bgs/*) --no-fehbg
-dwmblocks &
+feh --bg-scale "$(shuf -n1 -e ~/bgs/*)" --no-fehbg
+if ! pgrep dwmblocks >/dev/null ; then
+    dwmblocks &
+fi
+
 alacritty &
 setxkbmap -option caps:swapescape
 
 # Notifications
-dunst & 
+
+if ! pgrep dunst >/dev/null ; then
+    dunst & 
+fi
+
+
+
